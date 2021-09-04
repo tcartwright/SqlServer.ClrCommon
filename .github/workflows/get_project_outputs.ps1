6d71e4569bbc201b9f7c9b042901a11a77d830c2
Tim Cartwright <tcartwright@users.noreply.github.com>
2021-09-04 11:45:14 -0500

    Create get_project_outputs.ps1

---
+++ .github/workflows/get_project_outputs.ps1
@@ -0,0 +1,44 @@
+Param(
+    [Parameter(Mandatory=$true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [string] $ProjectPath, 
+    [string] $Configuration = "Release",
+    [string] $Platform = "AnyCPU",
+    [string] $TargetFramework = "^net.*"
+)
+Clear-Host
+
+[xml]$csproj = (Get-Content -Path $ProjectPath -Raw) 
+
+$framework = $csproj.Project.PropertyGroup.TargetFramework -split ";|," | Where-Object { $_ -match $TargetFramework } | Select-Object -First 1
+
+if ([string]::IsNullOrWhiteSpace($framework)) {
+    throw "Invalid framework"
+    exit 1
+}
+
+$releaseConfig = $csproj.Project.PropertyGroup | Where-Object { $_.Condition -ieq  " '`$(Configuration)|`$(Platform)' == '$Configuration|$Platform' " }
+
+if ($releaseConfig) {
+    $outputPath = $releaseConfig.OutputPath
+} else {
+    # attempt 2, maybe they moved the a global PropertyGroup
+    $outputPath = $csproj.Project.PropertyGroup.OutputPath
+}
+
+if ([string]::IsNullOrWhiteSpace($outputPath)) {
+    throw "Could not determine the outputpath."
+    exit 1
+}
+
+$assemblyName = $csproj.Project.PropertyGroup.AssemblyName | Select-Object -First 1
+
+if ([string]::IsNullOrWhiteSpace($assemblyName)) {
+    # if not set, assume the project name
+    $assemblyName = [System.IO.Path]::GetFileNameWithoutExtension($ProjectPath) 
+}
+
+$outputPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($ProjectPath), $outputPath, $framework)
+$outputDll = [System.IO.Path]::Combine($outputPath, "$assemblyName.dll")
+
+echo "GITHUB_BUILD_OUTPUT_PATH=$outputPath" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+echo "GITHUB_BUILD_OUTPUT_DLL=$outputDll" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
